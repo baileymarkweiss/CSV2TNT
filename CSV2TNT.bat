d89190d47f2067f894d9fcfdd8f155044c4c0887
Bailey Weiss <baileymarkweiss@gmail.com>
2025-08-25 14:53:02 +0200

    Update CSV2TNT.bat

--- CSV2TNT.bat
+++ CSV2TNT.bat
@@ -1,4 +1,15 @@
 @echo off
 REM This batch file runs the CSV2TNT python code. Read the manual for more information.
+
+REM installs required packages
 pip install -r requirements.txt
+
+REM minimises cmd window
+if not "%Minimized%"=="" goto :Minimized
+set Minimized=True
+start /min cmd /C "%~dpnx0"
+goto :EOF
+:Minimized
+
+REM Runs python code
 pythonw.exe C:\Users\baile\OneDrive\Python\CSV2TNT.py
