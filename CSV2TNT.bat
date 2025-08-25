374c9a350636af8e68d90e753a506564dcb458c7
Bailey Weiss <baileymarkweiss@gmail.com>
2025-08-25 14:42:42 +0200

    Add files via upload

---
+++ CSV2TNT.bat
@@ -0,0 +1,4 @@
+@echo off
+REM This batch file runs the CSV2TNT python code. Read the manual for more information.
+pip install -r requirements.txt
+pythonw.exe C:\Users\baile\OneDrive\Python\CSV2TNT.py
